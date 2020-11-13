408c64e0645d1d39debf175189007d66c6f574a1
198cad <198cad@gmail.com>
2020-11-14 01:57:47 +0700

    mongoose string connection edited

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -2,16 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 import mongoose from "mongoose";
 
-const mLab = `mongodb://erpqu:password@103.112.163.253:27017/erpqu`;
+const helper = `
+mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}
+`;
 
 mongoose
-  // .connect(`${process.env.MONGO_URI_1}:${process.env.MONGO_PORT}/erpqu`, {
-  .connect(mLab, {
+  .connect(helper, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("mongodb connected");
+    console.log(`database connected ${process.env.MONGO_DB}@${process.env.MONGO_HOST}`);
   })
   .catch((err) => {
     console.log("Error saat membuat koneksi ke server mongodb");
@@ -20,14 +21,11 @@ mongoose
 const db = mongoose.connection;
 
 mongoose.connection.on("connected", function () {
-  console.log(
-    // "Mongoose default connection open to " + `${process.env.MONGO_URI_1}:${process.env.MONGO_PORT}`
-    "Mongoose default connection open to @ds035787.mlab.com:35787/erpqu"
-  );
+  console.log(`Mongoose default connection open to ${process.env.MONGO_DB}@${process.env.MONGO_HOST}`);
 });
 
 mongoose.connection.on("error", function (err) {
-  console.log("Mongoose default connection error: @ds035787.mlab.com:35787/erpqu");
+  console.log(`Mongoose default connection error: ${process.env.MONGO_DB}@${process.env.MONGO_HOST}`);
 });
 
 mongoose.connection.on("disconnected", function () {
